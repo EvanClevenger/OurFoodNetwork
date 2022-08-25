4f4804d0dc22807471520567145f469272041061
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2022-08-24 19:51:05 -0500

    added post-routes to api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,11 @@
 
 const router = require('express').Router();
 
+const postRoutes = require('')
 const userRoutes = require('./user_routes.js');
 
+
+router.use('posts', postRoutes);
 router.use('/users', userRoutes);
 //we didn't use the word users in anyof the user-routes? 
 //That's because in this file we take those routes and implement them to another router instance, prefixing them with the path /users at that time.
